e58a5fda4eff9746d84761cf6b925214a1d2a23c
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-01-09 21:40:47 +0100

    Chore: add product data model

---
+++ react-projects/ecommerce-product-page/src/ecommerce-product-page/components/data/product.ts
@@ -0,0 +1,7 @@
+export const product = {
+  id: 1,
+  name: "Fall Limited Edition Sneakers",
+  price: 125,
+  thumbnail:
+    "/src/ecommerce-product-page/assets/images/image-product-1-thumbnail.jpg",
+};
